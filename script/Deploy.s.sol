52f1f73f5aae9c33f82e71c64f0f3cb41c4080c2
BernardOnuh <onuhbernard4@gmail.com>
2026-04-25 07:26:28 +0100

    deploy: sample matches - Barcelona vs Atletico

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -11,9 +11,9 @@ contract DeployPredictEarn is Script {
         address cUSD = vm.envAddress("CUSD_MAINNET"); 
         vm.startBroadcast();
         PredictEarn predictearn = new PredictEarn(cUSD);
-        console.log("Sample match 1: Liverpool vs Tottenham");
-        console.log("Sample match 2: Newcastle vs Arsenal");
-        console.log("Sample match 3: Chelsea vs Man United");
+        console.log("Sample match 1: Barcelona vs Atletico");
+        console.log("Sample match 2: Real Madrid vs Sevilla");
+        console.log("Sample match 3: Valencia vs Villarreal");
         vm.stopBroadcast();
         console.log("PredictEarn deployed at:", address(predictearn));
     }
